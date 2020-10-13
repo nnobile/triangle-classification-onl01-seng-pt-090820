3bc10e079facb5db3a07a8845172a6d516e3b63f
Nick Nobile <nickcnobile@gmail.com>
2020-10-13 01:39:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,7 +18,7 @@ class Triangle
     #define what constitues an equilateral triangle
     if (@side1 == @side2) == (@side2 == @side3)
       return :equilateral
-    elsif (@side1 != @side2) == (@side2 != @side3)
+    elsif (@side1 != @side2) && (@side1 != @side3)
       return :scalene
 
 
