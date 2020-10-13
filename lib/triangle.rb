1f7d9b05369def51e79b756cf6f09a5fc54064df
Nick Nobile <nickcnobile@gmail.com>
2020-10-13 01:05:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,10 @@
 class Triangle
-  attr_accessor :length_side1, :length_side2, :length_side3
+  attr_accessor :side1, :side2, :side3
 
-  def initialize(length_side1, length_side2, length_side3)
-    @length_side1 = length_side1
-    @length_side2 = length_side2
-    @length_side3 = length_side3
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
   end
 
   def kind(isosceles, equilateral, scalene)
