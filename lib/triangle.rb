ccb39a45d03f80b8b3ceaeb43fd18b016fd457f9
Nick Nobile <nickcnobile@gmail.com>
2020-10-13 00:54:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,7 @@
 class Triangle
   attr_accessor :length_side1, :length_side2, :length_side3
 
-  def initialize(length_side1, length_side2, length_side3)
+  def initialize(length_side1:, length_side2:, length_side3:)
     @length_side1 = length_side1
     @length_side2 = length_side2
     @length_side3 = length_side3
